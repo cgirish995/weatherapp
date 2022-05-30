ce406ef509292bca8ff6dd05c0d1d7c08294167f
girish <girish.chandrashekar@unvired.io>
2022-05-30 18:25:14 +0530

    environmentProduction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+
+weatherApiBaseUrl:
+    'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '7b92e5a529mshffe2b4e848d72e1p1033cfjsn26f011f66687',
+
 };
